c49858ac76315b2b77fe0f724bdfe1e3eb049cd6
SoftestEngineer98 <tarafinahdestine@gmail.com>
2022-04-27 12:45:34 -0400

    inplumented the error message so it would show the page

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ async function pictureButtonHandler() {
     const pictureInput = document.querySelector('input')
     const url = 'https://api.unsplash.com/search/photos/?client_id=U593A96VvuX0wAYUrayYqBHxwVyASPltFOBkKrSctLk&query=' + pictureInput.value
 
-    const response = await fetch(url)
-    const data = await response.json()
-    const randomNumber = Math.floor(Math.random() * data.results.length);
-    console.log(data.results[randomNumber].alt_description)
-    document.querySelector('h2').innerText = data.results[randomNumber].alt_description
-    document.querySelector('img').src = data.results[randomNumber].urls.regular
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        const randomNumber = Math.floor(Math.random() * data.results.length);
+        console.log(data.results[randomNumber].alt_description)
+        document.querySelector('h2').innerText = data.results[randomNumber].alt_description
+        document.querySelector('img').src = data.results[randomNumber].urls.regular
 
+    } catch (error) {
+        
+    }
 }
 
 const pictureButton = document.querySelector('button')
