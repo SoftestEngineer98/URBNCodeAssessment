72d3a8bff7c2d0395bbdbf93d9e489f45ed9a817
SoftestEngineer98 <tarafinahdestine@gmail.com>
2022-04-27 12:39:38 -0400

    Wanted to inpulment the randomNumber so random images with their descriptions are pulled from the api

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ async function pictureButtonHandler() {
 
     const response = await fetch(url)
     const data = await response.json()
-    document.querySelector('h2').innerText = data.results[0].alt_description
-    document.querySelector('img').src = data.results[0].urls.regular
+    const randomNumber = Math.floor(Math.random() * data.results.length);
+    console.log(data.results[randomNumber].alt_description)
+    document.querySelector('h2').innerText = data.results[randomNumber].alt_description
+    document.querySelector('img').src = data.results[randomNumber].urls.regular
 
 }
 
