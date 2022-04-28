a11d05bb0ff6626f130c3bbf6f3c2dd94564db22
SoftestEngineer98 <tarafinahdestine@gmail.com>
2022-04-27 22:29:36 -0400

    Put in a check for no results

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ async function pictureButtonHandler() {
     const pictureInput = document.querySelector('input')
     const url = 'https://api.unsplash.com/search/photos/?client_id=U593A96VvuX0wAYUrayYqBHxwVyASPltFOBkKrSctLk&query=' + pictureInput.value
     console.log(url)
-        const span = document.getElementById ('errorMessage')
+    const span = document.getElementById('errorMessage')
 
     try {
         span.innerText = ''
         const response = await fetch(url)
         const data = await response.json()
-        const randomNumber = Math.floor(Math.random() * data.results.length);
-        console.log(data.results[randomNumber].alt_description)
-        document.querySelector('h2').innerText = data.results[randomNumber].alt_description
-        document.querySelector('img').src = data.results[randomNumber].urls.regular
+        if (data.results.length === 0) {
+            document.querySelector('h2').innerText = ''
+            document.querySelector('img').src = ''
+            span.innerText = 'No results found for search'
+            console.log('No results')
+        } else {
+            const randomNumber = Math.floor(Math.random() * data.results.length);
+            console.log(data.results[randomNumber].alt_description)
+            document.querySelector('h2').innerText = data.results[randomNumber].alt_description
+            document.querySelector('img').src = data.results[randomNumber].urls.regular
+        }
 
     } catch (error) {
         document.querySelector('h2').innerText = ''
